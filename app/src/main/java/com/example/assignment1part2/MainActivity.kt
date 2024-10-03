8d60c60a0465d7f4d298acefeca2d0d193d5e7b0
Sarah Malik <smallk@bu.edu>
2024-10-03 09:43:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/assignment1part2/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.assignment1part2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.assignment1part2.ui.theme.Assignment1Part2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Assignment1Part2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "World",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var displayed by remember {
+        mutableStateOf(value = false)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (displayed) {
+            Text(
+                text = "Hello $name",
+                fontSize = 40.sp,
+                modifier = Modifier.height(50.dp)
+            )
+        }
+        else{
+            Spacer(modifier = Modifier.height(50.dp)) // ensures consistent button position
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+            displayed = !displayed
+        }) {
+            Text(
+                text = "Greet me",
+                fontSize = 15.sp
+            )
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Assignment1Part2Theme {
+        Greeting("Android")
+    }
+}
